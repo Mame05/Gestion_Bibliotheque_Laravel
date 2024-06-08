cff8a188fcb58a15b0dd3394451dbb854a63e44f
mareme <mariamathiaw92@gmail.com>
2024-06-08 22:28:51 +0000

    création de la route /categorie/ajouter pour le formulaire d'ajout de bien, de la vue ajouter_com qui contient le formulaire et la route ajouter/categorie traitement pour l'enregistrement des données du foumulaire

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
 use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+    public function AjouterCategorie(){
+        return view ('categories/ajouter_cat');
+
+     }
+
+        public function AjouterCategorieTraitement(Request $request){
+        /*dd($request->all());*/
+        $request->validate([
+            'libelle' => 'required',
+            'description' => 'required',
+            
+        ]);
+
+        $categorie =new Categorie();
+        $categorie->libelle = $request->libelle;
+        $categorie->description = $request->description;
+        $categorie->save();
+
+        /*return redirect('/categorie')->with('status', "La categorie a été ajouté avec succés.");*/
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategorieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+    /* Routes pour ajouter une catégorie */
+Route::get('/categorie/ajouter', [CategorieController::class, 'AjouterCategorie']);
+Route::post('/ajouter/categorie-traitement', [CategorieController::class, 'AjouterCategorieTraitement']);
+      
